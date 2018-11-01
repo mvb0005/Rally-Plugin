a8d1ec006614ee33ed588781116a8945834ac7a9
Matt Bonsall <mvb0005@auburn.edu>
2018-10-31 21:40:47 -0600

    Updated Function.java

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -9,14 +9,18 @@ import com.microsoft.azure.functions.*;
  */
 public class Function {
     /**
-     * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpTrigger-Java&code={your function key}
-     * 2. curl "{your host}/api/HttpTrigger-Java?name=HTTP%20Query&code={your function key}"
-     * Function Key is not needed when running locally, to invoke HttpTrigger deployed to Azure, see here(https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook#authorization-keys) on how to get function key for your app.
+     * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke
+     * it using "curl" command in bash: 1. curl -d "HTTP Body" {your
+     * host}/api/HttpTrigger-Java&code={your function key} 2. curl "{your
+     * host}/api/HttpTrigger-Java?name=HTTP%20Query&code={your function key}"
+     * Function Key is not needed when running locally, to invoke HttpTrigger
+     * deployed to Azure, see
+     * here(https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook#authorization-keys)
+     * on how to get function key for your app.
      */
-    @FunctionName("HttpTrigger-Java")
-    public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+    @FunctionName("HttpTrigger")
+    public HttpResponseMessage run(@HttpTrigger(name = "req", methods = { HttpMethod.GET,
+            HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
@@ -25,7 +29,8 @@ public class Function {
         String name = request.getBody().orElse(query);
 
         if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                    .body("Please pass a name on the query string or in the request body").build();
         } else {
             return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
         }
